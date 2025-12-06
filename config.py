c928b77bbc7b8a67f25e3692f096ad141ea1943c
minyi817 <chiamy-pm24@student.tarc.edu.my>
2025-12-06 12:38:33 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cjvhgjredejm.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee_db"
+custombucket = "chiaminyi-bucket"
+customregion = "us-east-1"
+
